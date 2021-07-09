76b5278fa83a8017fb117fb94ab827a99278c81c
simeenshaikh <87196304+simeenshaikh@users.noreply.github.com>
2021-07-09 21:08:15 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,20 @@
+package week2;
+
+import java.util.Scanner;
+
+public class multiple {
+
+	public static void main(String[] args) {
+		Scanner Scan=new Scanner(System.in);
+				
+		System.out.println("Enter value for i...");
+		int i=Scan.nextInt();
+		System.out.println("Enter value of j...");
+		int j=Scan.nextInt();
+		int mul=i*j;
+		System.out.println("The multiplication of i and j is:"+mul);
+		
+			
+	}
+
+}
